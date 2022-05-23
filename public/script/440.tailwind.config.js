56efb41fcbeca146103e11c213a3438a55f9fe59
Konstantin <command.com@yahoo.com>
2022-05-23 21:39:08 +0300

    deploy webpage

---
+++ public/script/440.tailwind.config.js
@@ -0,0 +1,13 @@
+"use strict";
+(self["webpackChunkwebpack_pkj"] = self["webpackChunkwebpack_pkj"] || []).push([[440],{
+
+/***/ 440:
+/***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
+
+__webpack_require__.r(__webpack_exports__);
+// extracted by mini-css-extract-plugin
+
+
+/***/ })
+
+}]);
