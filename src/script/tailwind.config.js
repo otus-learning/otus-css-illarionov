de1bb3ce3e7081b77aaeef8a9203d476aa1a45b3
Konstantin <command.com@yahoo.com>
2022-05-30 22:43:41 +0300

    debug for deploy

--- src/script/tailwind.config.js
+++ src/script/tailwind.config.js
@@ -15,7 +15,6 @@ import "../pic/stgh2.png";
 import "../pic/stgh3.png";
 import "../pic/test.png";
 
-
 tailwind.config = {
 	theme: {
 		extend: {
@@ -28,11 +27,11 @@ tailwind.config = {
 				'vh18': '18vh',
 				'vh28': '28vh',
 				'vh37': '37vh',
-				'vh43':	'43vh',
+				/*'vh43':	'43vh',*/
 				'vh45': '45vh',
 				'vh50': '50vh',
 				'vh55': '55vh',
-				'vh63': '63vh',
+				/*'vh63': '63vh',*/
 				'vh72': '72vh',
 				'vh73': '73vh',
 				'vh80': '80vh',
@@ -44,15 +43,24 @@ tailwind.config = {
 				'%7': '7%',
 				'%9': '9%',
 				'%11': '11%',
+				'%13': '13%',
+				'%15': '15%',
 				'%20': '20%',
+				'%25': '25%',
 				'%28': '28%',
+				'%37': '37%',
 				'%40': '40%',
+				'%43': '43%',
 				'%53': '53%',
 				'%56': '56%',
+				'%63': '63%',
 				'%72': '72%',
 				'%76': '76%',
+				'%87': '87%',
 				'%92': '92%',
+				'%95': '95%',
 				'px221': '221px',
+				'px650': '650px',
 			},
 			fontFamily: {
 				sans: ['Roboto', 'sans-serif'],
@@ -77,6 +85,9 @@ tailwind.config = {
 				'bpX1': {'raw': '(max-width: 1200px)'},
 				'bpX2': {'raw': '(max-width: 900px)'},
 				'bpX3': {'raw': '(max-width: 425px)'},
+				'bpX4': {'raw': '(min-width: 450px)'},
+				'bpX5': {'raw': '(min-width: 620px)'},
+				'bpX6': {'raw': '(min-width: 900px)'},
 				'bpY0': {'raw': '(max-height: 899px)'},
 				'bpY1': {'raw': '(max-height: 810px)'},
 				'bpY2': {'raw': '(min-height: 1100px)'},
